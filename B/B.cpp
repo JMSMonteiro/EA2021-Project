e7070619dab4aea42a10d237384c700ee9d74e7b
JMMonteiro <monteirojmig@gmail.com>
2021-04-21 18:59:28 +0100

    Problem B | Better code readability

--- B/B.cpp
+++ B/B.cpp
@@ -10,39 +10,44 @@ uint arches = 0;
 
 
 // Que é que a gente faz com isto ??? xD
-int mod_abs(int a, int mod) {  return ((a % mod) + mod) % mod;}
-int mod_add(int a, int b, int mod) {  return (mod_abs(a, mod) + mod_abs(b, mod)) % mod;}
-int mod_sub(int a, int b, int mod) {  return mod_add(a, -b, mod);}
+int mod_abs(int a, int mod) {  
+    return ((a % mod) + mod) % mod;
+}
+int mod_add(int a, int b, int mod) {  
+    return (mod_abs(a, mod) + mod_abs(b, mod)) % mod;
+}
+int mod_sub(int a, int b, int mod) {  
+    return mod_add(a, -b, mod);
+}
 
-
-void buildArc(int n, int h, int max_h, std::vector<uint> arc, int current, bool direction) {
+void buildArc(int blockNumber, int blockHeight, int maxHeight, std::vector<uint> arc, int current, bool direction) {
     int cur;
 
-   if (current == n - 2){
+    if (current == blockNumber - 2){
         arches++;
         return;
     }
 
     // Refix for more than 1 block insertions
     if (direction) {
-        arc[current] = arc[current -1 ] + h - 1; 
+        arc[current] = arc[current - 1] + blockHeight - 1; 
     }   
     else {
-        arc[current] = arc[current - 1] - h + 1;
+        arc[current] = arc[current - 1] - blockHeight + 1;
     }
     
     // If it reaches the n-2 block without failing any rule an arc was successfully made
  
     cur = arc[current];
-    if (cur > max_h) {
+    if (cur > maxHeight) {
         return;
     }
-    if (cur == max_h) {
-        buildArc(n,h,max_h,arc,current + 1,DOWN); // Call with DOWN ??
+    if (cur == maxHeight) {
+        buildArc(blockNumber, blockHeight, maxHeight, arc, current + 1,DOWN); // Call with DOWN ??
     }     
     else {
-        buildArc(n,h,max_h,arc,current + 1,UP);
-        buildArc(n,h,max_h,arc,current + 1,DOWN);
+        buildArc(blockNumber, blockHeight, maxHeight, arc, current + 1, UP);
+        buildArc(blockNumber, blockHeight, maxHeight, arc, current + 1, DOWN);
     }
 }
 
@@ -50,24 +55,28 @@ void buildArc(int n, int h, int max_h, std::vector<uint> arc, int current, bool 
 
 
 int main() {
+    //Fast(er) C++ I/O | https://git.dei.uc.pt/snippets/25
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(0);
-    int t,n,h,max_h;
-    std::cin >> t;
+    //End of Fast(er) I/O
 
-    for(int i=0;i<t;++i){
-        std::cin>>n>>h>>max_h;
-        for(int j=2;j<n;++j){
+    int testCases, blockNumber, blockHeight, maxHeight;
+    std::cin >> testCases;
+
+    for (int i = 0; i < testCases; ++i){
+        std::cin >> blockNumber >> blockHeight >> maxHeight;
+        for (int j = 2; j < blockNumber; ++j){
             std::vector<uint> arc(j);
-            if(j==2) arches++;
+            if (j == 2) {
+                arches++;
+            }
             else{
                 arc[0] = 0;
-                arc[1] = h; // needs change if second and second to last can have more than 1 block
-                arc[j-2] = h;
+                arc[1] = blockHeight; // needs change if second and second to last can have more than 1 block
+                arc[j-2] = blockHeight;
                 arc[j-1] = 0;
-                buildArc(n,h,max_h,arc,2,UP);
+                buildArc(blockNumber, blockHeight, maxHeight, arc, 2, UP);
             }
-
         }
     }
 }
